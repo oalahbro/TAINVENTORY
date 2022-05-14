e5b604e6f89a9e781b33b706ec8d1bec16200bb5
evantoday <kreasievan@gmail.com>
2022-05-14 21:56:52 +0700

    fix mongo exposed

--- application/libraries/Mongodb.php
+++ application/libraries/Mongodb.php
@@ -2,7 +2,7 @@
 class Mongodb{
     function table($col) {
         $client = new MongoDB\Client("mongodb://ekacuk:404notfound@localhost:27017"); // koneksi ke mongo db
-        // $client = new MongoDB\Client('mongodb://evantoday:babybear55@cluster0-shard-00-00.s2hx8.mongodb.net:27017,cluster0-shard-00-01.s2hx8.mongodb.net:27017,cluster0-shard-00-02.s2hx8.mongodb.net:27017/test?replicaSet=atlas-tsmeyc-shard-0&ssl=true&authSource=admin');
+        
         $database = 'mongophp'; // nama database
         $collection = $client->$database->$col; //nama collection
         return $collection;
